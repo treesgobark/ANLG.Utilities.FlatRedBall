eb55eab2b6ff4c04e4c878e80bf95955363210a9
Logan Petruziello <loganpetro97@yahoo.com>
2022-12-17 00:49:13 -0500

    Added normalized input position and made both extensions work with nulls

--- ANLG.Utilities.FlatRedBall/Extensions/I2DInputExtensions.cs
+++ ANLG.Utilities.FlatRedBall/Extensions/I2DInputExtensions.cs
@@ -9,10 +9,31 @@ public static class I2DInputExtensions
     /// <summary>
     /// Allows deconstruction into (Vector2 position, Vector2 velocity, float magnitude)
     /// </summary>
-    public static void Deconstruct(this I2DInput input, out MgVector2 position, out MgVector2 velocity, out float magnitude)
+    public static void Deconstruct(this I2DInput? input, out MgVector2 position, out MgVector2 velocity, out float magnitude)
     {
+        if (input is null)
+        {
+            position = MgVector2.Zero;
+            velocity = MgVector2.Zero;
+            magnitude = 0f;
+            return;
+        }
+        
         position = new MgVector2(input.X, input.Y);
         velocity = new MgVector2(input.XVelocity, input.YVelocity);
         magnitude = input.Magnitude;
+    }
+
+    /// <summary>
+    /// Returns the normalized position of this input using I2DInput's X and Y properties.
+    ///   Returns <see cref="MgVector2.Zero"/> if magnitude is zero or if input is null.
+    /// </summary>
+    public static MgVector2 GetNormalizedPositionOrZero(this I2DInput? input)
+    {
+        return input switch
+        {
+            null or { Magnitude: 0 } => MgVector2.Zero,
+            _ => new MgVector2(input.X, input.Y) / input.Magnitude,
+        };
     }
 }
