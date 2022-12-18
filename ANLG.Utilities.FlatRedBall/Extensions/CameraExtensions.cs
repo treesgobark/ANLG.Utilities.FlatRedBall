e39e4d70938a76fab526f996be0aabf38a2d99fb
Logan Petruziello <loganpetro97@yahoo.com>
2022-12-18 15:02:59 -0500

    Forgot to make the camera method an extension method kek

--- ANLG.Utilities.FlatRedBall/Extensions/CameraExtensions.cs
+++ ANLG.Utilities.FlatRedBall/Extensions/CameraExtensions.cs
@@ -2,9 +2,13 @@ using FrbCamera = FlatRedBall.Camera;
 using MgVector2 = Microsoft.Xna.Framework.Vector2;
 namespace ANLG.Utilities.FlatRedBall.Extensions;
 
-public class CameraExtensions
+/// 
+public static class CameraExtensions
 {
-    public static (MgVector2 minEdges, MgVector2 maxEdges) AbsoluteEdgesAt(FrbCamera camera, float absoluteZ = 0)
+    /// <summary>
+    /// Returns the four absolute edges of this camera in the form of (Vector2(left, bottom), Vector2(right, top)).
+    /// </summary>
+    public static (MgVector2 minEdges, MgVector2 maxEdges) AbsoluteEdgesAt(this FrbCamera camera, float absoluteZ = 0)
     {
         return (
             new MgVector2(camera.AbsoluteLeftXEdgeAt(absoluteZ), camera.AbsoluteBottomYEdgeAt(absoluteZ)),
