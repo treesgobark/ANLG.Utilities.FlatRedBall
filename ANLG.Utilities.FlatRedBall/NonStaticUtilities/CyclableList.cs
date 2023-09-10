07a31a928c4845f96b71b5b1c62a55c90b6aa5e7
Logan Petruziello <loganpetro97@yahoo.com>
2023-09-10 18:41:22 -0400

    cyclable list

---
+++ ANLG.Utilities.FlatRedBall/NonStaticUtilities/CyclableList.cs
@@ -0,0 +1,29 @@
+using ANLG.Utilities.FlatRedBall.StaticUtilities;
+
+namespace ANLG.Utilities.FlatRedBall.NonStaticUtilities;
+
+public class CyclableList<T> : List<T>
+{
+    private int _currentIndex = 0;
+    
+    public T CurrentItem { get; private set; }
+
+    public T SetCurrentItem(int index)
+    {
+        _currentIndex = ValidateIndex(index);
+        return CurrentItem = this[_currentIndex];
+    }
+
+    public T CycleToNextItem() => SetCurrentItem(_currentIndex + 1);
+    public T CycleToPreviousItem() => SetCurrentItem(_currentIndex - 1);
+
+    private int ValidateIndex(int index)
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("List has no items.");
+        }
+
+        return index.Regulate(Count);
+    }
+}
