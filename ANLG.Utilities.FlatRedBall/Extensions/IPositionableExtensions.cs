b322a7c3d5f587885927b567103ba4549972e4d9
treesgobark <loganpetro97@yahoo.com>
2024-06-21 20:20:22 -0400

    some additional IPositionableExtensions.cs methods

--- ANLG.Utilities.FlatRedBall/Extensions/IPositionableExtensions.cs
+++ ANLG.Utilities.FlatRedBall/Extensions/IPositionableExtensions.cs
@@ -9,4 +9,14 @@ public static class IPositionableExtensions
     {
         return new Vector3(positionable.X, positionable.Y, positionable.Z);
     }
+    
+    public static Vector3 VelocityAsVec3(this IPositionable positionable)
+    {
+        return new Vector3(positionable.XVelocity, positionable.YVelocity, positionable.ZVelocity);
+    }
+    
+    public static Vector3 AccelerationAsVec3(this IPositionable positionable)
+    {
+        return new Vector3(positionable.XAcceleration, positionable.YAcceleration, positionable.ZAcceleration);
+    }
 }
