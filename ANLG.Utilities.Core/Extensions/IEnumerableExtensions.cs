f541b35df0289e8f90ab0631662a2854d4b02ccf
treesgobark <loganpetro97@yahoo.com>
2024-06-27 19:24:11 -0400

    choose random list element

---
+++ ANLG.Utilities.Core/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,19 @@
+namespace ANLG.Utilities.Core.Extensions;
+
+public static class IEnumerableExtensions
+{
+    /// <summary>
+    /// Returns a random element from the list
+    /// </summary>
+    public static T ChooseRandom<T>(this IReadOnlyList<T> list, Random? instance = null)
+    {
+        if (instance is null)
+        {
+            instance = Random.Shared;
+        }
+
+        int index = instance.Next(list.Count);
+
+        return list[index];
+    }
+}
