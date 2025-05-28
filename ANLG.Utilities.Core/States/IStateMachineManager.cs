4efcebf6f1ceeaae35ac1ed1adcdeaa9b89dd474
treesgobark <loganpetro97@yahoo.com>
2025-05-28 10:16:58 -0400

    add IStateMachineManager.cs

---
+++ ANLG.Utilities.Core/States/IStateMachineManager.cs
@@ -0,0 +1,20 @@
+namespace ANLG.Utilities.Core.States;
+
+public interface IStateMachineManager
+{
+    /// <summary>
+    /// Adds a state machine to the manager.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Throws if state machine is not initialized</exception>
+    void Add<TSearch>(IStateMachine stateMachine, bool isExact = false) where TSearch : IState;
+
+    /// <summary>
+    /// Performs <see cref="IStateMachine.DoCurrentStateActivity"/> on all contained state machines.
+    /// </summary>
+    void DoAllStateMachineActivity();
+    
+    /// <summary>
+    /// Uninitializes all contained state machines.
+    /// </summary>
+    void Uninitialize();
+}
